e9cc60e257d85fbaca4c3ac46af325f801490347
vtphatt2 <thinhphat544@gmail.com>
2024-06-02 21:57:14 +0700

    DataCollect/html_collect.py - get html source code into Data/source_file.html

---
+++ DataCollect/html_collect.py
@@ -0,0 +1,20 @@
+import requests
+from bs4 import BeautifulSoup
+
+def get_and_save_html(url, filename):
+    response = requests.get(url)
+    response.raise_for_status()  
+
+    soup = BeautifulSoup(response.content, 'html.parser')
+    html_content = soup.prettify() 
+
+    with open(filename, 'w', encoding='utf-8') as file:
+        file.write(html_content)
+
+    print(f"Successfully saved HTML content of {url} to {filename}")
+
+url = "https://batdongsan.vn/ban-nha/"  
+filename = "../Data/source_file.html"
+
+get_and_save_html(url, filename)
+
